d9c31cee1f13cae48a9e419f79a60ae49a50ba7a
Tao LI <litao.91@bytedance.com>
2018-06-09 13:49:50 +0800

    Exec pandoc
    Change-Id: I454016c718c2aac8ec0682087f38313b1e7bbb14

--- main.go
+++ main.go
@@ -1,22 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
 	"path"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 type MDServer struct {
-	host string
-	port int64
-	path string
+	host      string
+	port      int64
+	path      string
+	pandocCmd string
 }
 
 func (server *MDServer) handleReq(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	file := ps.ByName("file").Trim("/")
-	path.Join(server.path, file)
+	file := strings.Trim(ps.ByName("file"), "/")
+	filePath := path.Join(server.path, file)
+	cmdStr := fmt.Sprintf(server.pandocCmd, filePath)
+	fmt.Println("Command: " + cmdStr)
+	cmd := exec.Command("bash", "-c", cmdStr)
+	var stdout, err = cmd.StdoutPipe()
+	if err != nil {
+		fmt.Printf("%v", err)
+		return
+	}
+
+	if err = cmd.Start(); err != nil {
+		fmt.Printf("%v", err)
+		return
+	}
+
+	reader := bufio.NewReader(stdout)
+	for {
+		data, err := reader.ReadBytes('\n')
+		if err != nil {
+			fmt.Printf("%s", err)
+			return
+		}
+		w.Write(data)
+	}
 }
 
 func (server *MDServer) RunHTTPServer() (err error) {
@@ -30,15 +58,17 @@ func (server *MDServer) RunHTTPServer() (err error) {
 
 func NewServer(filePath string, port int64) (server *MDServer) {
 	server = &MDServer{
-		host: "127.0.0.1",
-		port: port,
-		path: filePath,
+		host:      "127.0.0.1",
+		port:      port,
+		path:      filePath,
+		pandocCmd: "pandoc -s --mathjax=http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML  --from=markdown+pipe_tables --to=html %s",
 	}
 	return
 
 }
 
 func main() {
-	server := NewServer("", 3333)
+	path := os.Args[1]
+	server := NewServer(path, 3333)
 	fmt.Printf("%v", server.RunHTTPServer())
 }
